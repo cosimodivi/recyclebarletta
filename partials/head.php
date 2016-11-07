c41be83e8bf5231cc1439457ba848055d685d143
cosimodivi <cosimodivi@gmail.com>
2016-11-07 19:07:32 +0100

    Added some basic and SEO meta tags

--- partials/head.php
+++ partials/head.php
@@ -1,11 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title><?php echo($title); ?></title>
-  <link rel="stylesheet" href="/assets/css/main.css">
-  <script type="text/javascript" src="/assets/js/app.js" async></script>
+	<!-- (START) Basic meta tag -->
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="theme-color" content="#FFF"> <!-- Android colore -->
+	<meta name="robots" content="noindex,nofollow">
+	<meta name="description" content="Rispondiamo ai tuoi dubbi sulla raccolta differenziata a Barletta">
+	<meta name="keywords" content="barletta, bt, raccolta differenziata, carta, plastica, vetro, umido, secco residuo, tetrapak, buttare">
+	<meta name="author" content="Puglia Software Open Source">
+	<title><?php echo($title); ?></title>
+	<!-- Basic meta tag (END) -->
+	
+	<!-- (START) Twitter-Facebook Meta Tag -->
+	<meta property="og:title" content="Recycle in Barletta - <?php echo $title; ?>">
+	<!-- Logo futuro <meta property="og:image" content=""> -->
+	<meta property="og:description" content="Non sai dove buttare un oggetto? Noi rispondiamo ai tuoi dubbi sulla raccolta differenziata a Barletta!">
+	<meta property="og:type" content="website">
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:title" content="Recycle in Barletta - <?php echo $title; ?>">
+	<meta name="twitter:description" content="Non sai dove buttare un oggetto? Noi rispondiamo ai tuoi dubbi sulla raccolta differenziata a Barletta!">
+	<!-- Twitter-Facebook Meta Tag (END) -->
+	
+
+	<link rel="icon" type="image/png" href="images/ico.png" />
+	<link rel="stylesheet" href="/assets/css/main.css">
+	<script type="text/javascript" src="/assets/js/app.js" async></script>
+	
 </head>
 <body>
