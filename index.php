09658fb7c48451bf310d17b005d737be48e2f55b
cosimodivi <cosimodivi@gmail.com>
2016-11-08 16:20:26 +0100

    Index page created

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php $title = 'Home'; require('./partials/head.php'); ?>
-
-<p>CSS is <b id="not">not</b> working</p>
-<p>JS is <b id="js-not">not</b> working</p>
-
-<hr>
-
-<form action="/result.php" method="GET">
-  Oggetto da riciclare
-  <input type="text" name="object">
-  <input type="submit" value="Submit">
-</form>
-
+	<div class="sezione1">
+		<div class="logo">
+			Recycle in <b>Barletta</b>
+		</div>
+		<div class="form">
+			<form method="GET" action="risultato.php">
+				<input required type="text" name="searchinput" placeholder="Inserisci qui il nome dell'oggetto che vuoi cercare" class="form-input">
+			</form>
+		</div>
+	</div>
+	<div class="sezione2">
+		<img src="./assets/images/recycle.png" class="responsiveimage" style="margin-top:10%;">
+	</div>
 <?php require('./partials/tail.php'); ?>
