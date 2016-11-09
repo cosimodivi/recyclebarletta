b6a036cfca00e0bb70e58803fff84c62f4bdccc7
cosimodivi <cosimodivi@gmail.com>
2016-11-09 19:35:00 +0100

    Basic layout implemented

--- result.php
+++ result.php
@@ -1,13 +1,20 @@
 <?php $title = 'Result'; require('./partials/head.php'); ?>
-
-<p>
-  Devi riciclare: <b><?php echo($_GET['object']); ?></b>
-</p>
-
-<form action="/email.php" method="POST">
-  Inserisci la tua email
-  <input type="text" name="email">
-  <input type="submit" value="Submit">
-</form>
-
+	<section class="section1">
+		<div class="logo">
+			<img src="./assets/images/recycle.png" class="responsiveimage" style="width:5%;">
+			Recycle in <b>Barletta</b>
+		</div>
+		<div class="content">
+			<p style="font-size:30px;text-align:center;">
+			  Devi riciclare <b><?php echo($_GET['searchinput']); ?></b><br>come: <b>Vetro</b>
+			</p>
+		</div>
+		<div class="description" style="text-align:center;">
+			Speriamo di averti aiutato. Clicca il pulsante Indietro per effettuare un'altra ricerca<br><br>
+			<a href="index.php" class="button" >Indietro</a>
+		</div>
+	</section>
+	<section class="section2" style="text-align:center;">
+		<img src="./assets/images/bin-glass.png" class="responsiveimage" style="width:50%;margin-top:100px;">
+	</section>
 <?php require('./partials/tail.php'); ?>
