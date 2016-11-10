80491c715e8d04f2b57bc893bb9732e108d85c68
cosimodivi <cosimodivi@gmail.com>
2016-11-10 17:16:02 +0100

    Email page created

--- email.php
+++ email.php
@@ -1,7 +1,20 @@
-<?php $title = 'Email'; require('./partials/head.php'); ?>
-
-<p>
-  Ti invieremo al più presto un'email a: <b><?php echo($_POST['email']); ?></b>
-</p>
-
-<?php require('./partials/tail.php'); ?>
+<?php $title = 'Result'; require('partials/head.php'); ?>
+	<section class="section1">
+		<div class="logo">
+			<img src="assets/images/recycle.png" class="responsiveimage logoimage">
+			Recycle in <b>Barletta</b>
+		</div>
+		<div class="content">
+			<p class="mdtext centertext">
+			  Devi riciclare <b><?php echo($_GET['searchinput']); ?></b><br>come: <b>Vetro</b>
+			</p>
+		</div>
+		<div class="description centertext">
+			Speriamo di averti aiutato. Clicca il pulsante Indietro per effettuare un'altra ricerca<br><br>
+			<a href="index.php" class="button flatwhite" >Indietro</a>
+		</div>
+	</section>
+	<section class="section2 centertext">
+		<img src="assets/images/bin-glass.png" class="responsiveimage sectionimage">
+	</section>
+<?php require('partials/tail.php'); ?>
