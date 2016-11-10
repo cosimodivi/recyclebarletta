f994515eac8a113d3590e047ae7dc4419aa64ed4
cosimodivi <cosimodivi@gmail.com>
2016-11-10 16:56:38 +0100

    Added a simple footer

--- partials/tail.php
+++ partials/tail.php
@@ -1,2 +1,10 @@
+    <section class="footer">
+        <p class="footerlinks">
+            <a href="#" class="footerlink">Chi siamo</a> <a href="#" class="footerlink">Contatti</a>
+        </p>
+        <p>
+            Copyright &copy; 2016 Puglia SOS - <a href="https://github.com/cosimodivi/recyclebarletta" target="_blank" class="whitelink">Check it on github!</a>
+        </p>
+    </section>
 </body>
 </html>
