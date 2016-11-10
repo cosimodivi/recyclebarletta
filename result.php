43e457ed3b665d360924de487b68b7ceeaa126ba
cosimodivi <cosimodivi@gmail.com>
2016-11-10 16:57:35 +0100

    Deleted all inline style

--- result.php
+++ result.php
@@ -1,20 +1,20 @@
 <?php $title = 'Result'; require('./partials/head.php'); ?>
 	<section class="section1">
 		<div class="logo">
-			<img src="./assets/images/recycle.png" class="responsiveimage" style="width:5%;">
+			<img src="./assets/images/recycle.png" class="responsiveimage logoimage">
 			Recycle in <b>Barletta</b>
 		</div>
 		<div class="content">
-			<p style="font-size:30px;text-align:center;">
+			<p class="mdtext centertext">
 			  Devi riciclare <b><?php echo($_GET['searchinput']); ?></b><br>come: <b>Vetro</b>
 			</p>
 		</div>
-		<div class="description" style="text-align:center;">
+		<div class="description centertext">
 			Speriamo di averti aiutato. Clicca il pulsante Indietro per effettuare un'altra ricerca<br><br>
-			<a href="index.php" class="button" >Indietro</a>
+			<a href="index.php" class="button flatwhite" >Indietro</a>
 		</div>
 	</section>
-	<section class="section2" style="text-align:center;">
-		<img src="./assets/images/bin-glass.png" class="responsiveimage" style="width:50%;margin-top:100px;">
+	<section class="section2 centertext">
+		<img src="./assets/images/bin-glass.png" class="responsiveimage binsimage">
 	</section>
 <?php require('./partials/tail.php'); ?>
