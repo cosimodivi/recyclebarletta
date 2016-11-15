8f34498126cbd7df2c5a89b7736388842edac63f
cosimodivi <cosimodivi@gmail.com>
2016-11-15 01:00:35 +0100

    Items array from items.php file

--- api/search.php
+++ api/search.php
@@ -1,27 +1,11 @@
 <?php
-const BIN_PLASTIC = 1;
-const BIN_PAPER = 2;
-const BIN_GLASS = 3;
-const BIN_ORGANIC = 4;
-const BIN_WET = 5;
-const BIN_BATTERIES = 6;
-const BIN_REST = 7;
+require('items.php');
 
-$items = [
-	'coca cola' => BIN_PLASTIC,
-	'fazzoletti' => BIN_PAPER,
-	'filo usb' => BIN_PLASTIC,
-	'polistirolo' => BIN_PLASTIC,
-	'tetrapak' => BIN_PLASTIC,
-	'aranciata' => BIN_PLASTIC,
-	'birra in vetro' => BIN_GLASS,
-	'birra in lattina' => BIN_PLASTIC
-];
-
+$search = strtolower($_GET['s']);
 
 foreach($items as $key => $v)
 {
-	if (strpos($key, $_GET['s']) !== false) {
+	if (strpos($key, $search) !== false) {
 		$result[] = array('name' => $key, 'value' => $v);
 	}
 }
